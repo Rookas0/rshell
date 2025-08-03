36a5e68e1e692670bf08158262ac3f035c9a9d4b
rookas06 <lukedgemmill@gmail.com>
2025-08-03 19:22:16 +0545

    tokenizer now gets type for string, word, and operator

--- src/tokenize/tokenize.c
+++ src/tokenize/tokenize.c
@@ -4,12 +4,14 @@
 #include <stdlib.h>
 
 #include "./tokenize.h"
-enum State { NORMAL, IN_QUOTE };
+#include "../list/list.h"
+
 
 
 /*** TOKENIZER ***/
 
 static const char *DELIMS = " \t\r\n";
+static const char *OPERATORS = "|><&";
 
 void add_token(struct token_list *list, struct token t) {
     if(list->size >= list->capacity) {
@@ -51,7 +53,7 @@ void init_token_list(struct token_list *tl)
     tl->size = 0;
 }
 
-void add_token_from_buff(struct token_list *tl, char *buf, size_t bufsize)
+void add_token_from_buff(struct token_list *tl, char *buf, size_t bufsize, enum type type)
 {
     if(strlen(buf) == 0) {
         return;
@@ -59,13 +61,14 @@ void add_token_from_buff(struct token_list *tl, char *buf, size_t bufsize)
     struct token t;
     fflush(stdout);
     t.value = strdup(buf);
+    t.type = type;
     memset(buf, 0, bufsize);
     add_token(tl, t);
 
 }
 
 struct token_list *tokenize(struct list *line) {//, size_t size) {
-    enum State state = NORMAL;
+    enum state state = NORMAL;
     long init_size = 4;
 
     struct token_list *tl = malloc(sizeof(struct token_list));
@@ -83,7 +86,7 @@ struct token_list *tokenize(struct list *line) {//, size_t size) {
         char c = cursor->c;
         if (state == NORMAL) {
             if(strchr(DELIMS, c)) {
-                add_token_from_buff(tl, buf, sizeof(buf));
+                add_token_from_buff(tl, buf, sizeof(buf), WORD);
                 i = 0;
                 // skip over rest of delims
                 while(cursor != NULL && strchr(DELIMS, c)) {
@@ -92,7 +95,21 @@ struct token_list *tokenize(struct list *line) {//, size_t size) {
                         c = cursor->c;
                     }
                 }
-            } else if (c == '"') {
+            } else if (strchr(OPERATORS, c)) {
+                add_token_from_buff(tl, buf, sizeof(buf), WORD);
+                i = 0;
+                buf[i++] = c;
+                if (cursor->next != NULL && cursor->next->c == c) {
+                    buf[i++] = c; 
+                    cursor = cursor-> next;
+                }
+                if (cursor->next != NULL) {
+                    cursor = cursor-> next;
+                }
+                buf[i] = '\0';
+                add_token_from_buff(tl, buf, sizeof(buf), OPERATOR);
+                i = 0;
+            }else if (c == '"') {
                 state = IN_QUOTE;
                 cursor = cursor->next;
             } else {
@@ -102,8 +119,8 @@ struct token_list *tokenize(struct list *line) {//, size_t size) {
             }
         } else if (state == IN_QUOTE) {
             if (c == '"') {
+                add_token_from_buff(tl, buf, sizeof(buf), STRING);
                 state = NORMAL;
-                add_token_from_buff(tl, buf, sizeof(buf));
                 if (cursor->next != NULL) {
                     cursor = cursor->next;
                     c = cursor->c;
@@ -121,7 +138,7 @@ struct token_list *tokenize(struct list *line) {//, size_t size) {
     }
 
     for (int i = 0; i < tl->size; i++) {
-        printf("Token %d: %s\r\n", i, tl->tokens[i].value);
+        printf("Token of type %d %d: %s\r\n", tl->tokens[i].type, i, tl->tokens[i].value);
     }
     free_list(line);
     return tl;
--- src/tokenize/tokenize.h
+++ src/tokenize/tokenize.h
@@ -1,7 +1,17 @@
 #pragma once
 #include "../list/list.h"
+
+enum state { NORMAL, IN_QUOTE };
+
+enum type {
+    STRING,
+    WORD,
+    OPERATOR,
+};
+
 struct token {
     char *value;
+    enum type type;
 };
 
 struct token_list {
@@ -10,6 +20,6 @@ struct token_list {
     int capacity;
 };
 void add_token(struct token_list *list, struct token t);
-void add_token_from_buff(struct token_list *tl, char *buf, size_t bufsize);
+void add_token_from_buff(struct token_list *tl, char *buf, size_t bufsize, enum type type);
 void free_tokens(struct token_list *list);
 struct token_list *tokenize(struct list *line);
