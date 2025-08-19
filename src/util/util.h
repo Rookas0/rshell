7cc770c062b1d30726312c8db3abe7d02675a509
rookas06 <lukedgemmill@gmail.com>
2025-08-19 18:40:46 +0545

    funcs pulled out

--- src/util/util.h
+++ src/util/util.h
@@ -1,9 +1,11 @@
 #pragma once
 
+#include <stdbool.h>
 struct string {
     int capac;
     int size;
     char *s;
 };
 
-static void init_string(struct string *str, int capac);
+void init_string(struct string *str, int capac);
+bool insert_char_to_str(struct string *str, char c, int pos);
